73de4f62acdbb7e7d5315408346e8a0d2bdb259f
bumsoo <meliketoy@gmail.com>
2018-03-05 13:47:54 +0900

    Commit #31 : Restore drawing box function

--- 4_detector/draw_bbox.py
+++ 4_detector/draw_bbox.py
@@ -3,6 +3,7 @@ import cv2
 import sys
 import csv
 import numpy as np
+import config as cf
 
 def check_and_mkdir(in_dir):
     if not os.path.exists(in_dir):
@@ -14,7 +15,7 @@ if __name__ == "__main__":
 
     for file_number in range(1, (27+1)):
         print("| Predicting Bounding Box for TEST%d..." %file_number)
-        original_img = cv2.imread('/home/bumsoo/Data/test/20_CELL_TEST/TEST%d/TEST%d.png' %(file_number, file_number))
+        original_img = cv2.imread("/home/bumsoo/Data/test/MICCAI_img/TEST%d.png" %file_number)
         mask_img = cv2.imread('./results/masks/TEST%d.png' %file_number)
 
         ret, threshed_img = cv2.threshold(cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY), 100, 255, cv2.THRESH_BINARY)
@@ -39,7 +40,7 @@ if __name__ == "__main__":
                 cv2.rectangle(original_img, (x,y), (x+w, y+h), (0,255,0), 2)
 
         # Ground truth (RED)
-        with open('/home/bumsoo/Data/test/Test_tight/TEST%d/TEST%d.csv' %(file_number, file_number)) as csvfile:
+        with open(cf.test_dir+'%d/TEST%d.csv' %(file_number, file_number)) as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 x, y, w, h = map(int, row[1:])
