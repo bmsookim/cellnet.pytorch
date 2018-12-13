27b28e1c3f2108e2edd358e662c4cb1b4b200cb5
bumsoo-dmis <meliketoy@gmail.com>
2018-12-14 01:03:36 +0900

    BCCD augment for image patches

--- 1_preprocessor/BCCD_augment_frcnn.py
+++ 1_preprocessor/BCCD_augment_frcnn.py
@@ -8,16 +8,18 @@ from xml.etree.ElementTree import parse
 from BCCD_frcnn_process import base_dir, image_dir, annot_dir, split_dir, train_txt, val_txt, test_txt
 from BCCD_frcnn_process import check_and_mkdir, parse_text, parse_XML, split_train_val, create_csv_to_txt
 
-def lst_to_img(patch_lst, path_img_lst, w, h, f_base, csvfile):
-    blank_canvas = np.zeros((w*3, h*3, 3))
-    mark_canvas = np.zeros((w*3, h*3, 3))
+patch_num_lst = [3, 4]
+
+def lst_to_img(patch_num, patch_lst, path_img_lst, w, h, f_base, csvfile):
+    blank_canvas = np.zeros((w*patch_num, h*patch_num, 3))
+    mark_canvas = np.zeros((w*patch_num, h*patch_num, 3))
     print(blank_canvas.shape)
 
     cnt = 0
-    file_path = os.path.join('./augmented', f_base+".jpg")
+    file_path = os.path.join('./augmented', f_base+"-%s.jpg" %patch_num)
     writer = csv.writer(csvfile, delimiter=',')
-    for i in range(3):
-        for j in range(3):
+    for i in range(patch_num):
+        for j in range(patch_num):
             starting_point = (w*i, j*h)
             blank_canvas[starting_point[0]:starting_point[0]+w, starting_point[1]:starting_point[1]+h] = patch_img_lst[cnt]
             mark_canvas[starting_point[0]:starting_point[0]+w, starting_point[1]:starting_point[1]+h] = patch_img_lst[cnt]
@@ -35,8 +37,24 @@ def lst_to_img(patch_lst, path_img_lst, w, h, f_base, csvfile):
                     cv2.rectangle(mark_canvas, (xmin, ymin), (xmax, ymax), (0,255,0), 2)
             cnt += 1
 
-    cv2.imwrite('./test/%s.jpg' %f_base, blank_canvas)
-    cv2.imwrite('./test_gt/%s.jpg' %f_base, mark_canvas)
+    cv2.imwrite('./test/%s-%d.jpg' %(f_base, patch_num), blank_canvas)
+    cv2.imwrite('./test_gt/%s-%d.jpg' %(f_base, patch_num), mark_canvas)
+
+def check_lst(test_lst):
+    for base in test_lst:
+        xml_dir = os.path.join(annot_dir, base+".xml")
+        root, WBC_lst = parse_XML(xml_dir)
+
+        cnt = 0
+        for element in root.findall('object'):
+            name = element.find('name').text
+            if (name == 'WBC'):
+                cnt += 1
+
+        if cnt == 0:
+            test_lst.remove(base)
+
+    return test_lst
 
 # train 205 samples
 if __name__ == "__main__":
@@ -57,16 +75,20 @@ if __name__ == "__main__":
     check_and_mkdir('./test_gt')
 
     with open('test.csv', 'w') as csvfile:
+        test_lst = check_lst(test_lst)
+        print(len(test_lst))
         for first_base in test_lst:
-            patch_lst = []
-            patch_lst.append(first_base)
-            tmp = test_lst
-            tmp.remove(first_base)
-            sample_lst = random.sample(tmp, 8)
+            for patch_num in patch_num_lst:
+                patch_lst = []
+                patch_lst.append(first_base)
+                #tmp = test_lst #call by reference
+                tmp = test_lst[:]
+                tmp.remove(first_base)
+                sample_lst = random.sample(tmp, patch_num**2-1)
 
-            patch_lst += sample_lst
-            patch_img_lst = list(map(lambda x : cv2.imread(os.path.join(image_dir, x+".jpg")), patch_lst))
-            print(len(patch_img_lst))
-            lst_to_img(patch_lst, patch_img_lst, w, h, first_base, csvfile)
+                patch_lst += sample_lst
+                patch_img_lst = list(map(lambda x : cv2.imread(os.path.join(image_dir, x+".jpg")), patch_lst))
+                print(len(patch_img_lst))
+                lst_to_img(patch_num, patch_lst, patch_img_lst, w, h, first_base, csvfile)
 
-    #create_csv_to_txt('train.csv', 'annotate.txt')
+    create_csv_to_txt('test.csv', 'annotate.txt')
